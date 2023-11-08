8ba19c28c442c2a603a165b64a5aa4098aa8a1ef
Akerke2 <149644475+Akerke2@users.noreply.github.com>
2023-11-08 09:18:46 +0600

    python home work

---
+++ strings.py
@@ -0,0 +1,249 @@
+# code = 'python'
+# print(code)
+# print(len(code)) #len деген канша элемент бар екенын корсетеды
+
+# коп текст болса """  тырнақша жазамыз
+# sen = """ffnowrusikejf[wj;k
+# nfnursepwuf jwpfvlfffwifh 
+# fjeo;i[vn ifpjcccrrrrusikkle ]]"""
+# print(sen)
+
+# f_name = "Akerke"
+# l_name = "Adikyzy"
+# space = " " # spce пробел ушын
+# full_name = f_name + space + l_name
+# print(full_name)
+# print(len(f_name))
+# print(len(l_name))
+# print(len(f_name) > len(l_name))
+# print(len(full_name))
+
+
+# print('I hope everyone is enjoying the jdjdjd dfjjd. \nRuslan') #\n кейінге элементтер автоматты түрде астыға түсіп кетеді 
+
+
+# print('day\tweek\tmonth') # \t автоматты түрде пробел калдырады
+# print('day 1\t5\t6\t')
+# print('day 1\t5\t6\t')
+# print('day 1\t5\t6')
+# print('day 1\t5\t6')
+
+# print("hello\"") # \ осы элементтын кою аркылы тырнакша коя аламыз
+
+
+# f_name = 'Akerke'
+# l_name = 'Adikyzy'
+# city = 'Almaty'
+# age = 15
+# format_str = 'I am {} {}. I am {}. I am {}'. format(f_name, l_name, city, age)
+# print(format_str)
+
+# a = 5 
+# b = 5 
+# print('{} + {} = {}'.format(a, b, a + b) )
+# print('{} // {} = {}'.format(a, b, a // b)) #// қалдықсыз бөлу
+
+
+# # fname = 'Akerke' 
+# lname = 'Adikyzy'
+# city = 'Almaty'
+# age = 15
+# print(f'I am {fname} {lname}. I am in {city}. I am {age}.')
+
+
+# name = 'Akerke'
+# a,b,c,d,f,g = name
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(f)
+# print(g)
+
+# city = 'Pariss'
+# letter = city[0]
+# print(letter)
+# secondlet = city[1]
+# print(secondlet)
+# lasrlet = len(city) - 1
+# lastletter = city[lasrlet]
+# print(lastletter)
+
+# language = 'python'
+# last_letter = language [-1]
+# print(last_letter) # n
+# second_last = language[-2]
+# print(second_last) #o print no
+
+# lili = 'ilovethismoment'
+# l = lili[0:3]
+# print(l)
+# o = lili[3:6]
+# print(o)
+# o = lili[-3:]
+# print(o)
+
+# rev_lili = lili[:: -1]
+# print(rev_lili)
+# pto = lili[0:15:3]
+# print(pto)
+
+# hw1
+
+# ddd = 'Thirty'
+# gg = "Days"
+# hh = "Of"
+# ss = "Python"
+# space = ' '
+# print(ddd + space + gg + space + hh + space + ss)
+
+# # 2
+# a = "Coding"
+# s = "for"
+# d = "All"
+# space = ' '
+# print(a + space + s + space + d)
+
+# 3
+# company =  "Coding For All"
+# print(company)
+# # 4
+# print(len(company))
+# # 5
+# print(company.upper())
+# # 6
+# print(company.lower())
+# # 7
+# print(company.capitalize())
+# # 8
+# print(company.title())
+# # 9
+# # print(company.slice(1))
+# print(company.index("All"))
+# # 10
+# print(company.find("For"))
+# # 11
+# print(company.replace("Coding For All", "Python"))
+# # 12
+# print("Python", "Python for ALL")
+# # 13
+# print(company.split())
+# # 14
+# kk = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" 
+# print(kk.split(","))
+# 15
+# kk = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" 
+# index = 0
+# print(kk[index])
+# 16
+# company =  "Coding For All"
+# 11 индекс
+# 17
+# kk = 55555555555
+# kk[4]
+# 18
+# abbreviation = ''.join(word[0] for word in "пайтон для всех".split())
+# print(abbreviation)
+
+
+# # 19\
+# abbreviation = ''.join(word[0] for word in "Кодирование для всех".split())
+# print(abbreviation)
+# # 20
+# r = "AABBCCDDD"
+# e = r.index("C")
+# print(e)
+
+# 21
+# G = "ADFFF"
+# H = G.index("F")
+# print(H)
+# # 22
+# g = ["coding for all"]
+# for user in g:
+#     position = g.rfind("l")
+#     print(position)
+
+# 22
+# gg = "Coding for all People"
+# gg.rfind("l")
+
+# 35
+# 23-24
+
+# sentence = "You cannot end a sentence with because because because is a conjunction"
+# dezz = sentence.rindex("because")
+# print(dezz)  #index index қайтарады find- индекс қайтарады
+
+# 25
+g = "mfvmvkvmkmvvmfkf;fpfkn rifrofn fi4fnofn nfo4ifni4 fioj4fi4fo mv"
+print(g[31:54]) # стрингтың кесіп алады 31 басталған элемент сосын 54 ке дейін қиады
+
+
+
+# 26
+# 9 символ
+# 27
+# 28
+# a = "Coding for All"
+# print(a.startswith("for"))
+
+
+#yes 
+# 29
+# 
+# 
+#  yes
+# 30
+# h = "Кодирование для всех"
+# j = h.strip()
+# print(j) # басындағы және соңындағы элементті алып тастайды
+# 31 
+# lili = "30DaysOfPython"
+# rr =  lili.isidentifier()
+# print(rr)  # Айнымалының аты бола ала ма соны тексеру үшін
+
+# jake = "thirty_days_of_python:"
+# jesica = jake.isidentifier()
+# print(jesica)
+
+# 32
+# lili = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+# result = '#'.join(lili)
+# print(result) #аралары не қою керек соны жазасың сол кещде арасына еңгізген эдементің шығады
+
+
+# # 33
+# print("I am enjoying this challenge. \nI just wonder what is next")
+# # 34
+# print("Name\t Age\t Country\t City\t Asabeneh\t 250\t Finland \tHelsinki")
+# 35
+# radius = 10 
+# plosh = 3.14 
+# results = 'The area of a circle with radius {} is {} meters {} sqare'. format(radius, plosh, 2)
+# print(results)                                                       
+
+# 36
+# a = 8 
+# d = 6 
+# k = f"{a} + {d} = {a + d}"
+# m = f"{a} - {d} = {a - d}"
+# o = f"{a} * {d} = {a * d}"
+# b = f"{a} / {d} = {a / d}"
+# module = f"{a} % {d} = {a % d}"
+# bolu = f"{a} // {d} = {a // d}"
+# polo = f"{a} ** {d} = {a ** d}"
+# print(k)
+# print(m)
+# print(o)
+# print(b)
+# print(module)
+# print(bolu)
+# print(polo)
+
+# str1 = "PYnative"
+# print(str1[1:4], str1[:5], str1[4:], str1[0:-1], str1[:-1])
+
+
+greeting = 'Hello, World!'
+print(greeting[1:10:2]) # !dlroW ,olleH
