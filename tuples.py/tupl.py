8e1f24119e416963fd9fb34431c9bc2af1d8e3d3
Akerke2 <149644475+Akerke2@users.noreply.github.com>
2023-11-08 20:57:21 +0600

    tuple hw

---
+++ tuples.py/tupl.py
@@ -0,0 +1,80 @@
+# Тьюплға ештеңе қосылмайды және алынбайды 
+# Бір элемент қосу үшін жаңа Тьюплға қосамыз
+# tpl = ("Abylai", "Akerke", "Ersultan")
+# tpl = tpl + ("Roza",)
+# print(tpl)
+
+
+#Масивте осылай қосамыз
+# lst = 
+
+
+# a = (4, 5)
+# b = (-2, -3)
+# d = ((b[0]-a[0])**2 + (b[1]-a[1])**2)**0.5
+# print(d)
+
+# personal = ("John John", 25, "Kazakhstan")
+# print(personal)
+
+
+#class work
+# 1
+# tpl = ()
+# # 2
+# br = tpl + ("Askar","Nurs","Isatay", "Muhammed")
+# print(br)
+# sisters = tpl + ("Bekzada", "Shakhzada", "Ulpan", "Ulzhan")
+# print(sisters)
+# # 3
+# brothers_sisters = br + sisters
+# print(brothers_sisters)
+# # 4
+# print(len(brothers_sisters))
+# # 5
+# md = ("Adi", "Kymbat")
+# print(md)
+# m_d = tpl + md
+# family_members = md + brothers_sisters
+# print(family_members)
+# # LEVEL2
+# family_members = ('Adi', 'Kymbat', 'Askar', 'Nurs', 'Isatay', 'Muhammed', 'Bekzada', 'Shakhzada', 'Ulpan', 'Ulzhan')
+# dady, momy, brother1, brother2, brother3, brother4, sister1, sister2, sister3, sister4, * rest = family_members
+# print(dady)
+# print(momy)
+# print(brother1)
+# print(brother2)
+# print(brother3)
+# print(brother4)
+# print(sister1)
+# print(sister2)
+# print(sister3)
+# print(sister4)
+# print(rest)
+# 2
+# fruit = ("mango", "banana", "apple","pineapple")
+# print(fruit)
+# vegetables = ("caret", "peas", "garlie", "cabbge", "mushroom")
+# print(vegetables)
+# meat = ("meat_horse", "meeat_cow", "meat_sheap")
+# print(meat)
+# food_stuff_tp = meat + vegetables + fruit
+# print(food_stuff_tp)
+# # 3
+# food_stuff_lt = list(food_stuff_tp)
+# print(food_stuff_lt)
+# print(tuple(food_stuff_lt))
+# # 4
+# print(food_stuff_lt[5:8])
+# # 5
+# print(food_stuff_lt[0:3])
+# print(food_stuff_lt[-3:])
+# # 6
+# del food_stuff_lt
+# 7
+# nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+# if "Estonia" in nordic_countries:
+#     print("'Estonia' in nordic_countries")
+# else: print("Estonia not in nordic")
+# if "Iceland" in nordic_countries:
+#     print("Iceland in nordic_countries")
